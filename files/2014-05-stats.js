f68aaf86608dd66da19dc9838884b0d5b8a02a37
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-22 10:31:13 -0700

    Add post.

---
+++ files/2014-05-stats.js
@@ -0,0 +1,24 @@
+google.load("visualization", "1", {packages:["corechart", "table"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+var data = google.visualization.arrayToDataTable([
+['Method'               , 'Executable size, KiB' , 'Stripped size, KiB' ],
+['printf'               ,   41                   ,  30],
+['IOStreams'            ,   84                   ,  62],
+['C++ Format'           ,   46                   ,  34],
+['tinyformat'           ,  418                   , 386],
+['Boost Format'         ,  990                   , 923]
+]);
+
+var table = new google.visualization.Table(document.getElementById('table_div'));
+table.draw(data.clone(), {});
+
+var options = {
+  title: 'Executable size',
+  vAxis: {title: 'Method', titleTextStyle: {color: 'red'}}
+};
+
+var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+data.removeColumn(1);
+chart.draw(data, options);
+}
