a18d7581aabcb1ed1418d82f08a396ca0a690261
Victor Zverovich <victor.zverovich@gmail.com>
2019-11-25 10:36:53 -0800

    Update stats

--- files/2013-09-stats.js
+++ files/2013-09-stats.js
@@ -3,22 +3,21 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
 var data = google.visualization.arrayToDataTable([
 ['Method'                      , 'Time, s' , 'Time ratio' ],
-['fmt::format_int'              ,   0.140499,           1.0],
-['cppx::decimal_from'          ,   0.160447, 1.14197965822],
-['fmt::Writer'                 ,   0.170481, 1.21339653663],
-['karma::generate'             ,   0.217157, 1.54561242429],
-['strtk::type_to_string'       ,   0.381777, 2.71729336152],
-['karma::generate+std::string' ,   0.405444, 2.88574295902],
-['fmt::Writer+std::string'     ,   0.414739, 2.95190001352],
-['fmt::format'                 ,   0.443754, 3.15841393889],
-['ltoa'                        ,   0.538002, 3.82922298379],
-['fmt::format+std::string'     ,   0.686678, 4.88742268628],
-['sprintf'                     ,   0.948262, 6.74924376686],
-['boost::lexical_cast'         ,    1.08146, 7.69727898419],
-['sprintf+std::string'         ,    1.20853, 8.60169823273],
-['std::stringstream'           ,    1.42531, 10.1446273639],
-['std::to_string'              ,     1.5242, 10.8484757899],
-['boost::format'               ,    4.43679, 31.5788012726]
+['fmt::format_int'             ,   0.127698,           1.0],
+['cppx::decimal_from'          ,   0.158876, 1.24415417626],
+['fmt::compile'                ,   0.166164, 1.30122633087],
+['karma::generate'             ,   0.167268, 1.30987172861],
+['itostr'                      ,   0.207244, 1.62292283356],
+['karma::generate+std::string' ,   0.256794, 2.01094770474],
+['fmt::format_to'              ,    0.29158, 2.28335604316],
+['boost::lexical_cast'         ,   0.393476, 3.08130119501],
+['fmt::format'                 ,   0.420751, 3.29489107112],
+['ltoa'                        ,   0.463599, 3.63043273975],
+['sprintf'                     ,   0.772874, 6.05235790694],
+['sprintf+std::string'         ,    0.87743,  6.8711334555],
+['std::to_string'              ,    1.03077, 8.07193534746],
+['std::stringstream'           ,    1.87598, 14.6907547495],
+['boost::format'               ,    3.75657, 29.4176102993],
 ]);
 
 var table = new google.visualization.Table(document.getElementById('table_div'));
