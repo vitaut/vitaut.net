29a6dc045ecb65d2e32873c2026c9ad4c8fb9bda
Denis Feklushkin <feklushkin.denis@gmail.com>
2026-08-10 16:37:19 +0300

    shift right: bits_per_iteration = 29

--- static/files/simple-dtoa-benchmark.cc
+++ static/files/simple-dtoa-benchmark.cc
@@ -99,7 +99,8 @@ struct decimal {
       }
       bigits[num_bigits++] = v % bigit_bound;
       int i = 0;
-      int bits_per_iteration = 9; // 10**9 can only be shifted left 9 bits.
+	  // 10**9 can only be shifted right 9 digits, nearest lower binary is 2^29
+      int bits_per_iteration = 29;
       for (; i - bits_per_iteration >= exp; i -= bits_per_iteration)
         shift_right(bits_per_iteration);
       if (i != exp) shift_right(i - exp);
